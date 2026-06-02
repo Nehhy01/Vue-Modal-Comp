2b3449c2c4443cfabb1eecef46f0a8578c923b2a
Nehhy <olanehemiah2006@gmail.com>
2026-06-03 00:36:26 +0100

    Built another Modal

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
-  <h1> Hello, I'm Learning Vue</h1>
+  <h1>Hello, I'm Learning Vue</h1>
   <h4>Vue is Fun</h4>
-  <Form placeholder="Enter name"/>
-  <Form placeholder="Enter age"/>
-  <Modal heading="Hey, I'm a Modal" text="Modals are pop-ups used to pass an information and are triggered by an event"/>
+  <Form placeholder="Enter name" />
+  <div v-if="showModal">
+    <Modal :theme="theme" @close="handleClick">
+      <template v-slot:links>
+        <a href="#">Learn more</a>
+      </template>
+      <template v-slot:socials>
+        <p>Contact us (LinkedIn)</p>
+      </template>
+      <h1>Hey, I'm a Modal</h1>
+      <p>Modals are pop-ups used to pass an information and are triggered by an event</p>
+    </Modal>
+  </div>
+  <button class="btn" @click="handleClick">Show Modal</button>
+
+  <div v-show="showModal2">
+    <Modal theme="dark" @close="handleClick2">
+      <h1>Hey, I'm the Second Modal</h1>
+      <p>Modals can be used to collect form data quickly also, its fun learning vue</p>
+
+      <template v-slot:socials>
+        <p>Click here to <a href="#">Visit my Website</a></p>
+      </template>
+    </Modal>
+  </div>
+
+  <button @click="handleClick2">Show Modal Two</button>
 </template>
 
 <script>
-import Form from './components/Form.vue'
-import Modal from './components/Modal.vue'
+import Form from "./components/Form.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  
-  components: {
-     Form, Modal
-  }
-}
-</script> 
+  name: "App",
+  data() {
+    return {
+      theme: "light",
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.showModal = !this.showModal;
+    },
+
+    handleClick2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+  components: { Form, Modal },
+};
+</script>
 
 <style>
 #app {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,46 +1,57 @@
 <template>
-    <div class="backdrop">
-
-    <div class="modal">
-    <h1>{{ heading }}</h1>
-    <p>Modals are pop-ups used to pass an information and are triggered by an event</p>
+  <div class="backdrop" @click.self="handleClick">
+    <div class="modal" :class="{ dark: theme == 'dark', light: theme == 'light' }">
+      <slot></slot>
+      <slot name="socials"></slot>
+      <slot name="links"></slot>
     </div>
-    </div>
-  
-  
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Modal',
-  props: [heading],
+  name: "Modal",
+  props: ["heading", "text", "theme"],
   data() {
-    return {
-
-    }
-  }
-}
+    return {};
+  },
+  methods: {
+    handleClick() {
+      this.$emit("close");
+    },
+  },
+};
 </script>
 
 <style>
 .backdrop {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    display: flex;
-    align-items: start;
-    justify-content: center;
-    background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.5);
 }
 
 .modal {
-    padding: 2rem;
-    background: white;
-    color: black;
-    border-radius: 5px;
-    width: 300px;
-    }
+  margin-top: 5rem;
+  padding: 2rem;
+  background: white;
+  color: black;
+  border-radius: 5px;
+  width: 300px;
+}
 
+.modal.dark {
+  background: rgb(61, 1, 1);
+  color: rgb(216, 216, 216);
+}
+
+.modal.light {
+  background: rgb(255, 252, 200);
+  color: rgb(14, 0, 95);
+}
 </style>
